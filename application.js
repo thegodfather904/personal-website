8f4ff65e0577187985e4151eaf228c923d9fc4c2
Tony Scialo <arscialo@gmail.com>
2016-02-06 21:00:09 -0500

    initial project version

---
+++ application.js
@@ -0,0 +1,92 @@
+$(document).ready(function(){
+	addClickEvents();
+	//loopThroughTitleWords();
+	
+	new WOW().init();
+});
+
+
+function addClickEvents(){
+	menuClick();
+	pulsateClick();
+}
+
+function menuClick(){
+	$('.menu-button').click(function(){
+		var menu = $('.menu' );
+		if(!menu.hasClass('menu-open')){
+			menu.animate({
+				marginRight: '+=400px'
+			}, 350, function() {
+			});
+			
+			menu.addClass('menu-open');
+			$('.menu-button').addClass('menu-button-open');
+		}
+		else{
+			menu.animate({
+				marginRight: '-=400px'
+			}, 350, function(){	
+			});
+			menu.removeClass('menu-open');
+			$('.menu-button').removeClass('menu-button-open');
+			$('.menu-button').addClass('menu-button-start');
+		}
+	});
+}
+
+function pulsateClick(){
+	$('.pulse-icon-wrapper').click(function(e){
+		$("body, html").animate({ 
+			scrollTop: $('#aboutMe').offset().top 
+		}, 500);
+	});
+}
+
+
+/*Loops through the 'beautiful' synonyms*/
+function loopThroughTitleWords(){
+	
+	var wordArray = ["elegant", "dynamic", "emotive"];
+	
+	var allSpans = "";
+	for(var i = 0; i < wordArray.length; i++){
+		allSpans += "<span class = 'hidden-title-word'>" + wordArray[i] + "</span>";
+	}
+	
+	//add the word spans to the div
+	var $beautifulWrapper = $('#beautifulWrapper');
+	$beautifulWrapper.html(allSpans);
+	
+	var $beautifulWrapper = $('#beautifulWrapper span');
+	
+	var needToShow = 0;
+	var currentlyShowing = wordArray.length - 1;
+	
+	setInterval(function(){
+		
+		$beautifulWrapper.eq(currentlyShowing).fadeOut();
+		$beautifulWrapper.eq(needToShow).fadeIn();
+		
+		if(currentlyShowing == wordArray.length - 1)
+			currentlyShowing = 0;
+		else
+			currentlyShowing++;
+		
+		if(needToShow == wordArray.length - 1)
+			needToShow = 0;
+		else
+			needToShow++;
+			
+		
+	}, 3000);
+
+	
+}
+
+
+
+
+
+
+
