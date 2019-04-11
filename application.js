b38c5408e85e06280d6e2ce7ab10b02b335aeba0
Scialo, Tony <tony_scialo@csx.com>
2019-04-11 16:34:11 -0400

    Updated skills section to include new skills

--- application.js
+++ application.js
@@ -1,32 +1,32 @@
-$(document).ready(function(){
+$(document).ready(function () {
 	addClickEvents();
 	//loopThroughTitleWords();
 });
 
 
-function addClickEvents(){
+function addClickEvents() {
 	menuClick();
 	pulsateClick();
 	showMoreSkillsClick();
 	menuLinkClick();
 }
 
-function menuLinkClick(){
-	$('#menuHome').click(function(e){
+function menuLinkClick() {
+	$('#menuHome').click(function (e) {
 		slidePage('#home');
 	});
-	$('#menuHello').click(function(e){
+	$('#menuHello').click(function (e) {
 		slidePage('#aboutMe');
 	});
-	$('#menuQuote').click(function(e){
+	$('#menuQuote').click(function (e) {
 		slidePage('#quoteSection');
 	});
-	$('#menuContact').click(function(e){
+	$('#menuContact').click(function (e) {
 		slidePage('#contactMe');
 	});
 }
 
-function slidePage(id){
+function slidePage(id) {
 	slideMenu();
 	$('html body').animate({
 		scrollTop: $(id).offset().top
@@ -34,69 +34,71 @@ function slidePage(id){
 }
 
 
-function menuClick(){
-	$('.menu-button').click(function(){
+function menuClick() {
+	$('.menu-button').click(function () {
 		slideMenu();
 	});
 }
 
-function slideMenu(){
-	var menu = $('.menu' );
-	if(!menu.hasClass('menu-open')){
+function slideMenu() {
+	var menu = $('.menu');
+	if (!menu.hasClass('menu-open')) {
 		menu.animate({
 			marginRight: '+=400px'
-		}, 350, function() {
-		});
+		}, 350, function () {});
 
 		menu.addClass('menu-open');
 		$('.menu-button').addClass('menu-button-open');
-	}
-	else{
+	} else {
 		menu.animate({
 			marginRight: '-=400px'
-		}, 350, function(){
-		});
+		}, 350, function () {});
 		menu.removeClass('menu-open');
 		$('.menu-button').removeClass('menu-button-open');
 		$('.menu-button').addClass('menu-button-start');
 	}
 }
 
-function pulsateClick(){
-	$('.pulse-icon-wrapper').click(function(e){
+function pulsateClick() {
+	$('.pulse-icon-wrapper').click(function (e) {
 		$("body, html").animate({
 			scrollTop: $('#aboutMe').offset().top
 		}, 500);
 	});
 }
 
-function showMoreSkillsClick(){
+function showMoreSkillsClick() {
 	var skillsLabel = $('.see-more-skills-label');
 	var showLess = 'show-less';
 
-	skillsLabel.click(function(){
+	skillsLabel.click(function () {
 
-		if(skillsLabel.hasClass(showLess)){
+		if (skillsLabel.hasClass(showLess)) {
 			$('.hidden-skills-section').slideUp();
 			skillsLabel.html('more');
-			skillsLabel.removeClass(showLess)
-		}
-		else{
+			skillsLabel.removeClass(showLess);
+			$('.about-me-container').animate({
+				'min-height': "-=400"
+			})
+		} else {
 			$('.hidden-skills-section').slideDown();
 			skillsLabel.html('less');
 			skillsLabel.addClass(showLess);
+			$('.about-me-container').animate({
+				'min-height': "+=400"
+			})
 		}
 	});
 }
 
 
 /*Loops through the 'beautiful' synonyms*/
-function loopThroughTitleWords(){
+function loopThroughTitleWords() {
 
 	var wordArray = ["elegant", "dynamic", "emotive"];
 
 	var allSpans = "";
-	for(var i = 0; i < wordArray.length; i++){
+	for (var i = 0; i < wordArray.length; i++) {
 		allSpans += "<span class = 'hidden-title-word'>" + wordArray[i] + "</span>";
 	}
 
@@ -109,17 +111,17 @@ function loopThroughTitleWords(){
 	var needToShow = 0;
 	var currentlyShowing = wordArray.length - 1;
 
-	setInterval(function(){
+	setInterval(function () {
 
 		$beautifulWrapper.eq(currentlyShowing).fadeOut();
 		$beautifulWrapper.eq(needToShow).fadeIn();
 
-		if(currentlyShowing == wordArray.length - 1)
+		if (currentlyShowing == wordArray.length - 1)
 			currentlyShowing = 0;
 		else
 			currentlyShowing++;
 
-		if(needToShow == wordArray.length - 1)
+		if (needToShow == wordArray.length - 1)
 			needToShow = 0;
 		else
 			needToShow++;
